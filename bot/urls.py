c05df4dd97182ab0a21f550fa79b8c91af620700
Alexiotovv <gpsalexvasquez@gmail.com>
2025-03-21 11:01:42 -0500

    fix_listajustificaciones

--- bot/urls.py
+++ bot/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include, re_path
 from . import views
+from .views import ListarJustificaciones
 
 urlpatterns = [  
     re_path('api/v1/login',views.login),
@@ -8,7 +9,7 @@ urlpatterns = [
     re_path("api/v1/whatsapp/", views.whatsapp, name="whatsapp"),
 
     # re_path('api/v1/list/justificaciones',views.list_justificaciones),
-    path('api/v1/list/justificaciones/', views.ListarJustificaciones, name='list_justificaciones'),
+    path('api/v1/list/justificaciones/', ListarJustificaciones.as_view(), name='list_justificaciones'),
 
     re_path('api/v1/delete/justificaciones/<justificacion_id>',views.delete_justificacion),
 
