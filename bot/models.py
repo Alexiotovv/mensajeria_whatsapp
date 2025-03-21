7f0060f3b90347418d50de3dbf589c13c0babc5f
Alexiotovv <gpsalexvasquez@gmail.com>
2025-03-21 11:26:32 -0500

    remove_unique_dni

--- bot/models.py
+++ bot/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 
 class Justificaciones(models.Model):
-    dni = models.CharField(max_length=8, unique=True)  # DNI como string de 8 caracteres
+    dni = models.CharField(max_length=8)  # DNI como string de 8 caracteres
     nombre = models.CharField(max_length=255)  # Nombre completo
     descripcion = models.TextField(blank=True, null=True)  # Descripción opcional
     foto_url = models.URLField(max_length=500, blank=True, null=True)  # URL de la foto
